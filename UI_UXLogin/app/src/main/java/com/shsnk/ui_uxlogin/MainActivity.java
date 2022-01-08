662285992a273b2ec6cac44a89fc0256a53a58f8
Shashank <shashanksaxena226@gmail.com>
2022-01-09 00:09:25 +0530

    Provide additional functionality to UI/UX program
    Toast message is added when click to Login and Registration button

--- UI_UXLogin/app/src/main/java/com/shsnk/ui_uxlogin/MainActivity.java
+++ UI_UXLogin/app/src/main/java/com/shsnk/ui_uxlogin/MainActivity.java
@@ -2,12 +2,32 @@ package com.shsnk.ui_uxlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView btn,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.textViewButton);
+        btn2=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Registration Button Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Additional functionality still need to be provide", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login BUtton Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Additional functionality still need to be provide", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
